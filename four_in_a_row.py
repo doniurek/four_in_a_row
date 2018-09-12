302c6af1f1d8b9178a40b4ba9c3042800f5456f6
doniurek <aldonaglebicka@gmail.com>
2018-09-12 16:27:31 +0200

    Improve print_board function - delete letters from fields

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,10 +1,10 @@
 board = [
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
-    ['1A', '2B', '3C', '4D', '5E', '6F', '7G'],
+    ['1', '2', '3', '4', '5', '6', '7'],
+    ['1', '2', '3', '4', '5', '6', '7'],
+    ['1', '2', '3', '4', '5', '6', '7'],
+    ['1', '2', '3', '4', '5', '6', '7'],
+    ['1', '2', '3', '4', '5', '6', '7'],
+    ['1', '2', '3', '4', '5', '6', '7'],
     ]
 
 def print_board(board):
@@ -12,8 +12,8 @@ def print_board(board):
     for line in range(len(board)):
         for field in range(len(board[line])):
             printable = printable + ' | ' + board[line][field]
-        printable = printable + ' | \n |____|____|____|____|____|____|____| \n |    |    |    |    |    |    |    | \n'
-    printable = '\n  __________________________________ \n |    |    |    |    |    |    |    | \n' + printable
+        printable = printable + ' | \n |___|___|___|___|___|___|___| \n |   |   |   |   |   |   |   | \n'
+    printable = '\n  ___________________________ \n |   |   |   |   |   |   |   | \n' + printable
     print(printable)
 
 print_board(board)
