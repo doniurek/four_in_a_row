9673886a4e082e636061057564327eabdc6c3aca
doniurek <aldonaglebicka@gmail.com>
2018-09-27 17:45:33 +0200

    Improve win function (diagonal lines)

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -36,18 +36,20 @@ def win(board):
             and board[row+2][column] == board[row+3][column]:
                 return True
 
-    l = []
-    for i in range(6):
-        l += board[i]
-
     # \
-    for i in range(18):
-        if l[i] == l[i+8] and l[i+8] == l[i+16] and l[i+16] == l[i+24]:
-            return True
+    for row in range(3):
+        for column in range(4):
+            if board[row][column] == board[row+1][column+1] \
+            and board[row+1][column+1] == board[row+2][column+2] \
+            and board[row+2][column+2] == board[row+3][column+3]:
+                return True
     # /
-    for i in range(24):
-        if l[i] == l[i+6] and l[i+6] == l[i+12] and l[i+12] == l[i+18]:
-            return True
+    for row in range(3,6):
+        for column in range(4):
+            if board[row][column] == board[row-1][column+1] \
+            and board[row-1][column+1] == board[row-2][column+2] \
+            and board[row-2][column+2] == board[row-3][column+3]:
+                return True
 
     return False
 
@@ -56,53 +58,55 @@ def gamer_info_string(gamer):
         return "gracz nr. 1 (@)"
     return "gracz nr. 2 (#)"
 
-board = [
-    [1, 2, 3, 4, 5, 6, 7],
-    [1, 2, 3, 4, 5, 6, 7],
-    [1, 2, 3, 4, 5, 6, 7],
-    [1, 2, 3, 4, 5, 6, 7],
-    [1, 2, 3, 4, 5, 6, 7],
-    [1, 2, 3, 4, 5, 6, 7],
-    ]
 
-instruction = 'Four in a row to gra dla dwóch osób.\
-\nPierwszy gracz wybiera kolumnę, do której wrzuca swój "żeton" (O albo X).\
-\nŻeton zajmuje najniższą pozycję w kolumnie.\
-\nGracze wrzucają swoje żetony na przemian, aż jeden z nich ułoży cztery żetony\
-\nw poziomie, pionie lub ukosie.\
-\nWygrywa ten gracz, który zrobi to jako pierwszy.\
-\nJeżeli natomiast plansza się zapełni, a nie utworzy się żadna czwórka,\
-\njest remis.'
+if __name__ == "__main__":
+    board = [
+        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7],
+        ]
 
-print(instruction)
+    instruction = 'Four in a row to gra dla dwóch osób.\
+    \nPierwszy gracz wybiera kolumnę, do której wrzuca swój "żeton" (O albo X).\
+    \nŻeton zajmuje najniższą pozycję w kolumnie.\
+    \nGracze wrzucają swoje żetony na przemian, aż jeden z nich ułoży cztery żetony\
+    \nw poziomie, pionie lub ukosie.\
+    \nWygrywa ten gracz, który zrobi to jako pierwszy.\
+    \nJeżeli natomiast plansza się zapełni, a nie utworzy się żadna czwórka,\
+    \njest remis.'
 
-print_board(board)
-
-for round_ in range(1, 43):
-    if round_ % 2 !=0:
-        gamer = "@"
-    else:
-        gamer = "#"
-
-    print(gamer_info_string(gamer).capitalize())
-
-    try:
-        position = int(input("Podaj numer kolumny, do której chcesz wrzucić żeton: "))
-    except ValueError:
-        position = 0
-
-    while not check_move(board, position):
-        try:
-            position = int(input("Niepoprawny numer kolumny, spróbuj jeszcze raz: "))
-        except ValueError:
-            position = 0
-
-    set_token(board, gamer, position)
+    print(instruction)
 
     print_board(board)
 
-    if round_ > 6 and win(board):
-        print("Koniec gry. Wygrywa", gamer_info_string(gamer)+". Gratulacje!" )
-        exit()
+    for round_ in range(1, 43):
+        if round_ % 2 !=0:
+            gamer = "@"
+        else:
+            gamer = "#"
 
-print("Koniec gry. Remis!")
+        print(gamer_info_string(gamer).capitalize())
+
+        try:
+            position = int(input("Podaj numer kolumny, do której chcesz wrzucić żeton: "))
+        except ValueError:
+            position = 0
+
+        while not check_move(board, position):
+            try:
+                position = int(input("Niepoprawny numer kolumny, spróbuj jeszcze raz: "))
+            except ValueError:
+                position = 0
+
+        set_token(board, gamer, position)
+
+        print_board(board)
+
+        if round_ > 6 and win(board):
+            print("Koniec gry. Wygrywa", gamer_info_string(gamer)+". Gratulacje!" )
+            exit()
+
+    print("Koniec gry. Remis!")
